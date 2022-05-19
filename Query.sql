b9ee3b98ad6d7fcf745ac7b956e4f477b04680a1
Marcato Luca <luca.marcato.2@studenti.unipd.it>
2022-05-19 20:02:03 +0200

    Scritte query 1-2-3 (ancora da definire)

---
+++ Query.sql
@@ -0,0 +1,46 @@
+--1
+SELECT Prodotto.Nome, Prodotto.Prezzo, Prodotto.Descrizione, Prodtto.Prime
+FROM Prodotto JOIN 
+(
+SELECT Prodotto as Codice, SUM(*) as NumeroResi
+FROM Reso
+GROUP BY Prodotto
+HAVING NumeroResi >= 1000
+) as ProdottoReso
+ON Prodotto.Codice = ProdottoReso.Codice
+
+--2
+SELECT Nome, NumeroTelefono, Email, ProdottiPrime
+FROM Fornitore JOIN
+(
+    SELECT Fornitore as PIVA, SUM(*) as ProdottiPrime
+    FROM Prodotto
+    WHERE Prime = TRUE
+    GROUP BY PIVA
+    ORDER BY ProdottiPrime DESC
+    --LIMIT(10) da aggiungere se si vogliono i primi 10
+) as FornitorePrime
+ON Fornitore.PIVA = FornitorePrime.PIVA
+
+--3
+DROP VIEW IF EXISTS FornitoreEstero
+CREATE VIEW FornitoreEstero as
+SELECT DISTINCT PIVA
+FROM Fornitore, Stabilimento
+WHERE Stabilimento.Fornitore = Fornitore.PIVA
+AND Stato <> "IT"
+
+SELECT ProdottiAcquistati.Utente
+FROM Prodotto JOIN
+(
+    SELECT ProdottiSalvati.Prodotto as Prodotto, ProdottiSalvati.Utente as Utente
+    FROM Ordine, Carrello, ProdottiSalvati
+    WHERE Ordine.Carrello = Carrello.id
+    AND Ordine.Utente = Carrello.Utente
+    AND Carrello.id = ProdottiSalvati.Carrello
+    AND Carrello.Utente = ProdottiSalvati.Utente
+) as ProdottiAcquistati
+ON ProdottiAcquistati.Prodotto = Prodotto.Codice
+WHERE Prodotto.Fornitore IN FornitoreEstero
+
+--4
