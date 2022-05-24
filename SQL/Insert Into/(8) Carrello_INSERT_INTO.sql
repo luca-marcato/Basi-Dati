287967267257db8a22f85f26385432f6f4c9dec8
Marcato Luca <luca.marcato.2@studenti.unipd.it>
2022-05-24 13:13:54 +0200

    Aggiunti INSERT INTO

--- SQL/Insert Into/(8) Carrello_INSERT_INTO.sql
+++ SQL/Insert Into/(8) Carrello_INSERT_INTO.sql
@@ -9,4 +9,101 @@ VALUES (1, 'utente2@studente.unipd.it', 616.41);
 INSERT INTO public.carrello(id, utente, importo)
 VALUES (2, 'utente2@studente.unipd.it', 12.95);
 INSERT INTO public.carrello(id, utente, importo)
-VALUES (1, 'utente3@studente.unipd.it', 576,22);
+VALUES (1, 'utente3@studente.unipd.it', 576.22);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente4@studente.unipd.it', 329.99);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (2, 'utente4@studente.unipd.it', 271.96);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (3, 'utente4@studente.unipd.it', 266.36);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente5@studente.unipd.it', 1116.39);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente6@studente.unipd.it', 356.47);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (2, 'utente6@studente.unipd.it', 353.77);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (3, 'utente6@studente.unipd.it', 991.45);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (4, 'utente6@studente.unipd.it', 108.64);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente7@studente.unipd.it', 256.30);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (2, 'utente7@studente.unipd.it', 410.00);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (3, 'utente7@studente.unipd.it', 0);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente8@studente.unipd.it', 441.13);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente9@studente.unipd.it', 0);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente10@studente.unipd.it', 195.97);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (2, 'utente10@studente.unipd.it', 111.57);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente11@studente.unipd.it', 99.93);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (2, 'utente11@studente.unipd.it', 384.97);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (3, 'utente11@studente.unipd.it', 187.18);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente12@studente.unipd.it', 288.25);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente13@studente.unipd.it', 232.83);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (2, 'utente13@studente.unipd.it', 346.32);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (3, 'utente13@studente.unipd.it', 265.82);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (4, 'utente13@studente.unipd.it', 1058.11);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente14@studente.unipd.it', 452.00);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (2, 'utente14@studente.unipd.it', 203.80);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente15@studente.unipd.it', 358.94);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente16@studente.unipd.it', 0);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente17@studente.unipd.it', 202.40);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (2, 'utente17@studente.unipd.it', 934.98);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente1@unipd.it', 175.75);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (2, 'utente1@unipd.it', 0);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente2@unipd.it', 293.67);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (2, 'utente2@unipd.it', 115.69);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (3, 'utente2@unipd.it', 0);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente3@unipd.it', 0);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente4@unipd.it', 20.45);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (2, 'utente4@unipd.it', 322);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente5@unipd.it', 97.94);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (2, 'utente5@unipd.it', 123.20);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (3, 'utente5@unipd.it', 132.91);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente6@unipd.it', 0);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente7@unipd.it', 0);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente8@unipd.it', 4.75);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (2, 'utente8@unipd.it', 189.70);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (3, 'utente8@unipd.it', 5.99);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (4, 'utente8@unipd.it', 44.43);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente9@unipd.it', 30.22);
+INSERT INTO public.carrello(id, utente, importo)
+VALUES (1, 'utente10@unipd.it', 20.45);
+
---
+++ SQL/Insert Into/(9) ProdottiSalvati_INSERT_INTO.sql
@@ -0,0 +1,192 @@
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0136', 1, 'utente1@studente.unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0307', 1, 'utente1@studente.unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0447', 1, 'utente1@studente.unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0147', 2, 'utente1@studente.unipd.it', 2);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0267', 2, 'utente1@studente.unipd.it', 2);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0467', 2, 'utente1@studente.unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0267', 3, 'utente1@studente.unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0397', 3, 'utente1@studente.unipd.it', 4);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0185', 1, 'utente2@studente.unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0156', 1, 'utente2@studente.unipd.it', 2);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0567', 1, 'utente2@studente.unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0447', 1, 'utente2@studente.unipd.it', 3);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0297', 2, 'utente2@studente.unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0376', 1, 'utente3@studente.unipd.it', 2);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0295', 1, 'utente3@studente.unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0115', 1, 'utente4@studente.unipd.it', 2);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0225', 1, 'utente4@studente.unipd.it', 3);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0437', 2, 'utente4@studente.unipd.it', 4);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0537', 2, 'utente4@studente.unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0257', 2, 'utente4@studente.unipd.it', 3); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0417', 3, 'utente4@studente.unipd.it', 2); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0307', 3, 'utente4@studente.unipd.it', 2); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0517', 1, 'utente5@studente.unipd.it', 5); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0337', 1, 'utente5@studente.unipd.it', 3); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0467', 1, 'utente5@studente.unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0367', 1, 'utente5@studente.unipd.it', 4); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0297', 1, 'utente6@studente.unipd.it', 7); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0226', 1, 'utente6@studente.unipd.it', 2); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0295', 2, 'utente6@studente.unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0155', 2, 'utente6@studente.unipd.it', 3); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0275', 2, 'utente6@studente.unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0195', 3, 'utente6@studente.unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0225', 4, 'utente6@studente.unipd.it', 2);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0427', 4, 'utente6@studente.unipd.it', 2);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0527', 1, 'utente7@studente.unipd.it', 5);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0237', 1, 'utente7@studente.unipd.it', 2); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0197', 2, 'utente6@studente.unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0337', 1, 'utente8@studente.unipd.it', 2); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0447', 1, 'utente8@studente.unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0557', 1, 'utente8@studente.unipd.it', 3); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0477', 1, 'utente8@studente.unipd.it', 3); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0427', 1, 'utente10@studente.unipd.it', 3); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0317', 1, 'utente10@studente.unipd.it', 2); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0247', 2, 'utente10@studente.unipd.it', 4); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0197', 2, 'utente10@studente.unipd.it', 3); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0257', 1, 'utente11@studente.unipd.it', 3);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0307', 1, 'utente11@studente.unipd.it', 2);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0115', 2, 'utente11@studente.unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0437', 2, 'utente11@studente.unipd.it', 3);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0537', 2, 'utente11@studente.unipd.it', 2);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0417', 3, 'utente11@studente.unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0225', 3, 'utente11@studente.unipd.it', 3); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0275', 1, 'utente12@studente.unipd.it', 3); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0245', 1, 'utente12@studente.unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0267', 1, 'utente12@studente.unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0367', 1, 'utente12@studente.unipd.it', 2); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0297', 1, 'utente13@studente.unipd.it', 10);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0155', 1, 'utente13@studente.unipd.it', 3); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0427', 1, 'utente13@studente.unipd.it', 2); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0295', 2, 'utente13@studente.unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0275', 2, 'utente13@studente.unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0226', 3, 'utente13@studente.unipd.it', 2); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0225', 4, 'utente13@studente.unipd.it', 2);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0195', 4, 'utente13@studente.unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0527', 1, 'utente14@studente.unipd.it', 4); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0197', 1, 'utente14@studente.unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0237', 2, 'utente14@studente.unipd.it', 2); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0337', 1, 'utente15@studente.unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0147', 1, 'utente15@studente.unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0557', 1, 'utente15@studente.unipd.it', 3); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0477', 1, 'utente15@studente.unipd.it', 3); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0327', 1, 'utente17@studente.unipd.it', 2); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0317', 1, 'utente17@studente.unipd.it', 2); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0207', 2, 'utente17@studente.unipd.it', 5);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0106', 2, 'utente17@studente.unipd.it', 2);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0396', 1, 'utente1@unipd.it', 2);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0175', 1, 'utente1@unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0307', 1, 'utente2@unipd.it', 3); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0337', 1, 'utente2@unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0367', 2, 'utente2@unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0116', 2, 'utente2@unipd.it', 4);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0225', 2, 'utente2@unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0155', 1, 'utente4@unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0295', 2, 'utente4@unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0176', 2, 'utente4@unipd.it', 2); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0296', 1, 'utente5@unipd.it', 2);   
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0326', 1, 'utente5@unipd.it', 3);  
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0187', 2, 'utente5@unipd.it', 1);  
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0226', 3, 'utente5@unipd.it', 1);
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0287', 1, 'utente8@unipd.it', 5); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0357', 2, 'utente8@unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0377', 2, 'utente8@unipd.it', 2); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0166', 3, 'utente8@unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0215', 4, 'utente8@unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0296', 1, 'utente9@unipd.it', 1); 
+INSERT INTO public.prodottisalvati(prodotto, carrello, utente, quantita)
+VALUES ('P0155', 1, 'utente10@unipd.it', 1); 
